9f9701ca985b29e394c867a9f96fb34504ec55c3
Justin Kajome <111602480+kajomejustin@users.noreply.github.com>
2023-04-08 11:29:17 +0200

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,49 @@
-from collections import namedtuple
+import numpy as np
 import altair as alt
-import math
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+st.title('Kajome Learning Material :flag-rw:')
+st.header('st.write')
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Example 1
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+st.write(':sunglasses: Hello, **World!** :+1:')
+st.header('This is a header')
+st.header('A header with _italics_ :blue[colors] and emojis :sunglasses:')
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Example 2
 
+st.write(1234)
+st.markdown("Streamlit is **_really_ cool**. It's like drinking :coffee:")
+st.markdown("This text is :red[colored red], and this is **:blue[colored]** and bold.", help=('This will display information about a certain text or argument in a code'))
+st.markdown(":green[$\sqrt{x^2+y^2}=1$] is a Pythagorean identity. :pencil:")
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# Example 3
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+df = pd.DataFrame({
+     'first column': [1, 2, 3, 4],
+     'second column': [10, 20, 30, 40]
+     })
+st.write(df)
+st.caption('This is a string that explains something above.')
+st.caption('A caption with _italics_ :blue[colors] and emojis :sunglasses:')
 
-    points_per_turn = total_points / num_turns
+# Example 4
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Example 5
+
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
+
+# Example 6
+
+code = '''def hello():
+    print("Hello, Streamlit!")'''
+st.code(code, language='python', line_numbers=True)
